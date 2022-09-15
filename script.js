49a11d72ed839136c55b3460e7018b329e274d82
kimamovic21 <imamovic.kerim@gmail.com>
2022-09-15 14:26:36 +0200

    Responsive navbar
    index.html,
    style.css,
    script.js
    universe.jpg image

---
+++ script.js
@@ -0,0 +1,18 @@
+// JS Todo
+
+// select hamburger
+const hamburger = document.querySelector('.hamburger');
+
+// select nav-menu
+const navMenu = document.querySelector('.nav-menu');
+
+// add event listener
+hamburger.addEventListener('click', mobileMenu);
+
+// navmenu function
+// toggle active
+function mobileMenu() {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+}
+
